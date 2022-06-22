e0711071536dac59e45260764c8f10aa3fdfd74e
Carly Donais <carly.donais@gmail.com>
2022-06-22 10:11:41 -0400

    added functions to generate intern and engineer

--- develop/lib/Manager.js
+++ develop/lib/Manager.js
@@ -1,10 +1,8 @@
 const Employee=require("./Employee");
 
-class Manager{
+class Manager extends Employee{
     constructor(name,id,email,office){
-        this.name=name;
-        this.id=id;
-        this.email=email;
+        super(name,id,email);
         this.office=office;
         this.role='Manager';
     }
--- develop/src/generateHTML.js
+++ develop/src/generateHTML.js
@@ -1,23 +1,53 @@
-const generateHTML=(manager)=>{
 
-    return`<!DOCTYPE html>
+const generateHTML=()=>{
+
+    return `<!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0'>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Team Profile</title>
     </head>
     <body>
         <header>    
             <h1>My Team</h1>
         </header>    
-        <div id=manager class=card>
+    </body> 
+</html>           
+    `;
+};
+const generateManager=(manager)=>{
+    return `
+    <div id=manager class=card>
             <h2>${manager.name}</h2>
-            <h3>manager<h3>
+            <h3>Manager<h3>
             ${manager.id}
             ${manager.email}
             ${manager.office}
         </div>
-    </body>            
+        `;    
+};
+const generateEngineer=(engineer)=>{
+    return `
+    <div id=engineer class=card>
+    <h2>${engineer.name}</h2>
+    <h3>Engineer</h3>
+    ${engineer.id}
+    ${engineer.email}
+    ${engineer.github}
+`;
+};
+const generateIntern=(intern)=>{
+    return `
+    <div id=intern class=card>
+    <h2>${intern.name}<h/2>
+    <h3>Intern</h3>
+    ${intern.id}
+    ${intern.email}
+    ${intern.school}
     `;
 };
+generateManager();
+generateEngineer();
+generateIntern();
 module.exports=generateHTML;
